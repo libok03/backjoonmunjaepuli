12815c39bfe31b515e4deaf3a6f9ad5623a99cea
libok03 <libok1117@gmail.com>
2024-08-23 14:43:35 +0900

    success

--- class4/10830.py
+++ class4/10830.py
@@ -28,11 +28,16 @@ def multi(A,T):
             c[i][j]=dot % 1000
     return c
 
+# 이진 곱 구현하기
 def matrix_pow(matrix,power):
+    # 단위 행렬로, 이거랑 아무거나 곱하면 원래것이 답으로 나온다. 신기함
     result = [[1 if i ==j else 0 for j in range(N)] for i in range(N)]
+    # B가 0보다 클동안
     while power > 0:
         if power % 2 ==1:
+            # 만약 B가 홀수면 미리 저장해줌 이것도 신기함
             result = multi(result,matrix)
+        # 계속 제곱해주기
         matrix = multi(matrix,matrix)
         power //=2
     return result
@@ -41,6 +46,9 @@ ans = matrix_pow(matrix,B)
 
 for i in ans:
     print(" ".join(map(str,i)))
+########################################################
+# 아래는 내가 그냥 만들어본 행렬이다.                     #
+# 이진곱 대신 올린 내용인데 이걸로 풀면 틀린다. 왜지..?    #
 ########################################################
 lenB=len(str(bin(B)))-3
 
@@ -61,3 +69,9 @@ for i in memory[1:]:
 
 for i in ans:
     print(" ".join(map(str,i)))
+
+
+
+
+# (나) 는 이진법 연산을 얻었다!
+# 근데 이걸로 풀면 왠지 틀린다?
